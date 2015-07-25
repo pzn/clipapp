483b415bb7f62f749dcacea6cb36909d48481f70
pzn <christophe.le.1@gmail.com>
2015-07-25 17:40:04 -0400

    Two possibilities: "simple" (an simple in-memory map) or "database"

---
+++ src/main/java/ip/cl/clipapp/ClipAppProfile.java
@@ -0,0 +1,8 @@
+package ip.cl.clipapp;
+
+public final class ClipAppProfile {
+
+    public static final String SIMPLE   = "simple";
+    public static final String DATABASE = "database";
+
+}
---
+++ src/main/java/ip/cl/clipapp/model/ClipUrl.java
@@ -0,0 +1,43 @@
+package ip.cl.clipapp.model;
+
+import ip.cl.clipapp.ClipAppProfile;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.context.annotation.Profile;
+
+@Profile(ClipAppProfile.DATABASE)
+@Entity
+@Table(name = "clipurl")
+public class ClipUrl implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Integer           id;
+
+    private String            longUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+}
