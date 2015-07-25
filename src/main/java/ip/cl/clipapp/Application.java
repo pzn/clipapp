b50372ea48882ff8987ee57a473e406cac072378
pzn <christophe.le.1@gmail.com>
2015-07-25 17:58:49 -0400

    Hook Heroku with a PostgreSQL

--- src/main/java/ip/cl/clipapp/Application.java
+++ src/main/java/ip/cl/clipapp/Application.java
@@ -1,8 +1,16 @@
 package ip.cl.clipapp;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 
 @ComponentScan
 @SpringBootApplication
@@ -10,6 +18,23 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Profile(ClipAppProfile.HEROKU)
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                        .driverClassName("org.postgresql.Driver")
+                        .username(username)
+                        .password(password)
+                        .url(dbUrl)
+                        .build();
     }
 
 }
--- src/main/java/ip/cl/clipapp/ClipAppProfile.java
+++ src/main/java/ip/cl/clipapp/ClipAppProfile.java
@@ -4,5 +4,6 @@ public final class ClipAppProfile {
 
     public static final String SIMPLE   = "simple";
     public static final String DATABASE = "database";
+    public static final String HEROKU   = "heroku";
 
 }
