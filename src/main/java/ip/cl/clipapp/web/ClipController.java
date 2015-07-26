3af9d135da21482293fc3b7cd796ef9e46fe2c1b
pzn <christophe.le.1@gmail.com>
2015-07-26 11:40:36 -0400

    Simple UI (endpoints changed)

--- src/main/java/ip/cl/clipapp/web/ClipController.java
+++ src/main/java/ip/cl/clipapp/web/ClipController.java
@@ -5,28 +5,53 @@ import ip.cl.clipapp.service.ExtenderService;
 import ip.cl.clipapp.service.ShortenerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 public class ClipController {
+
+    private static final String TINY_URL_PARAM = "tinyUrl";
 
     @Autowired
     private ShortenerService shortenerService;
     @Autowired
     private ExtenderService  extenderService;
 
-    @RequestMapping(params = "u", method = RequestMethod.GET)
-    public String clip(@RequestParam(value = "u") String longUrl) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(ModelMap modelMap) {
+        modelMap.putIfAbsent(TINY_URL_PARAM, "");
+        return "index";
+    }
+
+    @RequestMapping(params = "u", method = RequestMethod.POST)
+    public @ResponseBody String clip(@RequestParam(value = "u") String longUrl) {
         return shortenerService.shorten(longUrl);
     }
 
-    @RequestMapping(value = "{tinyUrl}", method = RequestMethod.GET)
-    public String unclip(@PathVariable("tinyUrl") String tinyUrl) throws ClipAppException {
+    @RequestMapping(value = "{tinyUrl}", method = RequestMethod.POST)
+    public @ResponseBody String unclip(@PathVariable("tinyUrl") String tinyUrl) throws ClipAppException {
         return extenderService.extend(tinyUrl);
+    }
+
+    @RequestMapping(value = "{tinyUrl}", method = RequestMethod.GET)
+    public ModelAndView redirect(@PathVariable("tinyUrl") String tinyUrl, RedirectAttributes redirectAttrs) throws ClipAppException {
+        String longUrl = null;
+        try {
+            longUrl = extenderService.extend(tinyUrl);
+        }
+        catch (ClipAppException e) {
+            redirectAttrs.addFlashAttribute(TINY_URL_PARAM, tinyUrl);
+            return new ModelAndView("redirect:/");
+        }
+        return new ModelAndView("redirect:" + longUrl);
     }
 
 }
--- src/test/java/ip/cl/clipapp/web/ClipControllerTest.java
+++ src/test/java/ip/cl/clipapp/web/ClipControllerTest.java
@@ -4,15 +4,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import ip.cl.clipapp.Application;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,30 +32,42 @@ public class ClipControllerTest {
     @Value("${local.server.port}")
     private int port;
     
+    private static final MultiValueMap<String, Object> PARAM_MAP = new LinkedMultiValueMap<>(1); 
     private final RestTemplate REST_TEMPLATE = new RestTemplate();
+
+    @BeforeClass
+    public static void beforeClass() {
+        PARAM_MAP.add("u", GOOGLE_COM);
+    }
 
 	@Test
 	public void clipUrl() {
-		ResponseEntity<String> response = REST_TEMPLATE.getForEntity("http://localhost:" + port + "/?u=" + GOOGLE_COM, String.class);
+	    ResponseEntity<String> response = REST_TEMPLATE.postForEntity("http://localhost:" + port, PARAM_MAP, String.class);
 		assertThat(response.getBody(), equalTo(GOOGLE_COM_SHORT));
 	}
 
     @Test
     public void unclipUrl() {
         // Add URL
-        REST_TEMPLATE.getForEntity("http://localhost:" + port + "/?u=" + GOOGLE_COM, String.class);
+        REST_TEMPLATE.postForEntity("http://localhost:" + port, PARAM_MAP, String.class);
         // Now the URL exists
         // We can retrieve it
-        ResponseEntity<String> response = REST_TEMPLATE.getForEntity("http://localhost:" + port + "/" + GOOGLE_COM_SHORT, String.class);
+        ResponseEntity<String> response = REST_TEMPLATE.postForEntity("http://localhost:" + port + "/" + GOOGLE_COM_SHORT, null, String.class);
         assertThat(response.getBody(), equalTo(GOOGLE_COM));
     }
 
     @Test(expected = HttpServerErrorException.class)
     public void unclipUrlNotFound() {
         // This time, we won't add the URL
-        ResponseEntity<String> response = REST_TEMPLATE.getForEntity("http://localhost:" + port + "/" + GOOGLE_COM_SHORT, String.class);
+        ResponseEntity<String> response = REST_TEMPLATE.postForEntity("http://localhost:" + port + "/" + GOOGLE_COM_SHORT, null, String.class);
         // Error excepted
         response.getBody();
+    }
+
+    @Test
+    public void index() {
+        ResponseEntity<String> response = REST_TEMPLATE.getForEntity("http://localhost:" + port, String.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
     }
 
 }
