0a462f449f30b09380ceb091be2d9f9117462ba1
Christophe Lé <pzn@users.noreply.github.com>
2018-01-20 11:40:09 -0500

    Refactoring ShortenerServiceImplTest

--- src/main/java/ip/cl/clipapp/service/impl/ShortenerServiceImpl.java
+++ src/main/java/ip/cl/clipapp/service/impl/ShortenerServiceImpl.java
@@ -3,26 +3,25 @@ package ip.cl.clipapp.service.impl;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
-import ip.cl.clipapp.ClipAppRuntimeException;
 import ip.cl.clipapp.service.LookupUrlService;
 import ip.cl.clipapp.service.ShortenerService;
+
+import static org.springframework.util.Assert.hasText;
 
 @Service
 public class ShortenerServiceImpl implements ShortenerService {
 
-    private UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https"});
-
+    private final UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https"});
     @Autowired
     private LookupUrlService lookupUrlService;
 
     @Override
     public String shorten(String longUrl) {
 
-        Assert.hasText(longUrl);
+        hasText(longUrl, "long url cannot be null or empty!");
         if (!urlValidator.isValid(longUrl)) {
-            throw new ClipAppRuntimeException("url is invalid");
+            throw new IllegalArgumentException("url is invalid");
         }
 
         return lookupUrlService.getOrAddLongUrl(longUrl);
