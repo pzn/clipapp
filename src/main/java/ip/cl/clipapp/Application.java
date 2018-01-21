f300f45c5b9e2cec8bddae1fa51a72b595a67f7d
Christophe Lé <pzn@users.noreply.github.com>
2018-01-20 21:50:13 -0500

    Spring Boot 1.5.9.RELEASE and Simplifying App Profiles

--- src/main/java/ip/cl/clipapp/Application.java
+++ src/main/java/ip/cl/clipapp/Application.java
@@ -1,44 +1,30 @@
 package ip.cl.clipapp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import static ip.cl.clipapp.ClipAppProfile.DATABASE;
 import static ip.cl.clipapp.ClipAppProfile.HEROKU;
-import static ip.cl.clipapp.ClipAppProfile.SIMPLE;
 
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Profile(SIMPLE)
+    @Profile(HEROKU)
     @Configuration
-    @EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
-    public static class SimpleClipAppConfiguration {
+    public static class DatabaseConfiguration {
 
-    }
-
-    @Profile(DATABASE)
-    @Configuration
-    @EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
-    public static class DbClipAppConfiguration {
-
-        @Profile(HEROKU)
         @Bean
         public DataSource dataSource() throws URISyntaxException {
             URI dbUri = new URI(System.getenv("DATABASE_URL"));
